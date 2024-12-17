c4f239ec198edcc991af84c74b8d176014bedea3
mlj <7284232+utuana@users.noreply.github.com>
2024-12-16 23:40:03 -0600

    changed to zsh

--- config/domains.lua
+++ config/domains.lua
@@ -7,12 +7,19 @@ return {
 
    -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
    wsl_domains = {
+      -- {
+      --    name = 'WSL:Ubuntu',
+      --    distribution = 'Ubuntu',
+      --    username = 'kevin',
+      --    default_cwd = '/home/kevin',
+      --    default_prog = { 'fish', '-l' },
+      -- },
       {
-         name = 'WSL:Ubuntu',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'fish', '-l' },
+         name = 'WSL:arch',
+         distribution = 'Arch Linux',
+         username = 'mlj',
+         default_cwd = '/home/mlj',
+         default_prog = { 'zsh', '-l' },
       },
    },
 }
